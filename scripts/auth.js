cb476e1789a120763ceb68820ccf4c00f812563e
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-09-08 21:22:35 +0700

    loginForm

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,10 +1,45 @@
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
+// signup
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
 
-  var items = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(items);
+  console.log(email, password);
 
+  //signup up the user
+
+  auth.createUserWithEmailAndPassword(email, password).then(() => {
+    const modal = document.querySelector("#modal-signup");
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("user signed out");
+  });
+});
+
+// login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred);
+    // close the login modal and reset the form
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset;
+  });
 });
